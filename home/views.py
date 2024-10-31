4e074eb1a65ba145a9bdb217b9c9a66570b912dc
roshin <roshinjamesjohn009@gmail.com>
2024-10-31 20:18:08 +0530

    5 http methods implemented

--- home/views.py
+++ home/views.py
@@ -19,10 +19,43 @@ def home(request):
         return Response(post_resp)
     
 
-@api_view(['GET','POST'])
+@api_view(['GET','POST','PUT','PATCH','DELETE'])
 def peoples(request):
+
     if request.method=="GET":
         people_data=people_details.objects.all()
         serialized_data=people_serializer(people_data,many=True)
-        return Response(serialized_data.data if serialized_data.data else serialized_data.errors) 
+        return Response(serialized_data.data if serialized_data.data else serialized_data.errors)
     
+    elif request.method=="POST":
+        new_data=request.data
+        serialized_data=people_serializer(data=new_data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data)
+        return Response(serialized_data.errors)
+    
+    elif request.method=="PUT":
+        data=request.data
+        obj=people_details.objects.get(id=data['id'])
+        updated_data=people_serializer(obj,data=data,partial=False)
+        if updated_data.is_valid():
+            updated_data.save()
+            return Response(updated_data.data)
+        return Response(updated_data.errors)
+    
+    elif request.method=="PATCH":
+        data=request.data
+        obj=people_details.objects.get(id =data['id'])
+        serialized_data=people_serializer(obj,data=data,partial=True)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data)
+        return Response(serialized_data.errors)
+    
+    else:
+        data=request.data
+        obj=people_details.objects.get(id=data['id'])
+        if obj.delete():
+            return Response({"status":"deleted"})
+        return Response({"status":"failed"})
