580324e71adedf9a9c12f8fb8a108e1366fc5bad
roshin <roshinjamesjohn009@gmail.com>
2024-10-31 23:45:02 +0530

    nested object in json

--- home/models.py
+++ home/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+class team(models.Model):
+    team_name=models.CharField(max_length=30)
+    team_color=models.CharField(max_length=20)
+
+    class Meta:
+        db_table="team"
 
 class people_details(models.Model):
     name=models.CharField(max_length=20,blank=False)
     gender=models.CharField(max_length=10,blank=False)
     place=models.CharField(max_length=50,blank=False)
     age=models.PositiveIntegerField(default=25)
+    team=models.ForeignKey(team,on_delete=models.CASCADE,null=True,blank=True,default=None)
 
     class Meta:
         db_table="people_details"
--- home/serializers.py
+++ home/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from .models import *
 
+class team_serializer(serializers.ModelSerializer):
+    class Meta:
+        model=team
+        fields=['team_name']
 
 class people_serializer(serializers.ModelSerializer):
+    team=team_serializer()
     class Meta:
         model=people_details
         fields='__all__'
+        depth=1
+
+    #for the validation of the input data
+    def validate(self, data):
+        special_chars="~!@#$%^&*()_+<>:/|{}[];,.?/"
+        if any(x in special_chars for x in data['name']):
+            raise serializers.ValidationError("Name should not have special characters!")
+        if data['age']<18:
+            raise serializers.ValidationError("Person should have age greater than 18!")
+        return data
