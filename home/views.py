c121a336cc2f08d930bd094c94689136bf3a5453
roshin <roshinjamesjohn009@gmail.com>
2024-10-31 13:40:39 +0530

    first_test

--- home/views.py
+++ home/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET','POST'])
+def home(request):
+    if request.method=="GET":
+        get_resp={
+            'name':'roshin',
+            'lname':'james',
+            'age':'24'
+        }
+        return Response(get_resp)
+    elif request.method=="POST":
+        post_resp={
+            'method':'post'
+        }
+        return Response(post_resp)
